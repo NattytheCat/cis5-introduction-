10a967db7e82b59639c5bf1be8a27c5fb666b412
NattytheCat <nataliejackmartinez41@gmail.com>
2026-01-07 12:37:27 -0800

    Create Intro.cpp

---
+++ Intro.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int age;
+
+cout << "Enter your age: ";
+cin >> age;
+
+cout << "\n--- Introduction ---\n";
+cout << "Name: Natalie Martinez\n";
+cout << "Goals in Life: Become an artist and educator\n";
+cout << "Current Interests: Art history, design, programming\n";
+cout << "Dreams: Teaching at a college and running my own studio\n";
+cout << "In 1 year: Transferring to a university\n";
+cout << "In 3 years: Completing my degree\n";
+cout << "In 5 years: Working professionally\n";
+cout << "In 7 years: Teaching and mentoring students\n";
+
+return 0;
+}
